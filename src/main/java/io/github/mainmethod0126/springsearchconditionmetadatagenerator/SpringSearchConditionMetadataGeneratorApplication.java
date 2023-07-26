6ef210a9f0c05c30512030a33d45036a3718c577
wusub.shin@softcamp.co.kr <wusub.shin@softcamp.co.kr>
2023-07-26 16:42:03 +0900

    first commit

---
+++ src/main/java/io/github/mainmethod0126/springsearchconditionmetadatagenerator/SpringSearchConditionMetadataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package io.github.mainmethod0126.springsearchconditionmetadatagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSearchConditionMetadataGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSearchConditionMetadataGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mainmethod0126/springsearchconditionmetadatagenerator/SpringSearchConditionMetadataGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mainmethod0126.springsearchconditionmetadatagenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSearchConditionMetadataGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
